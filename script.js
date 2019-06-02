cd38fd0485265ca6d6c2f8c75700a82625c188d1
Sirena Alyce Web Design <48938630+SirenaAlyceWD@users.noreply.github.com>
2019-06-02 11:33:32 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+//  $(".right").mouseover(function(){
+//    $(".right").css("background-color", "yellow");
+//  });
+//  $(".right").mouseout(function(){
+//    $(".right").css("background-color", "lightgray");
+//  });
+});
+
+
+//if right is mousedover then pause the left
+//
+//if left is mousedover then pause the right
+
